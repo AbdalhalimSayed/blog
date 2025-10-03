d0b96b40ea353f742f31c5ff87859aae79618d2a
AbdalhalimSayed <sydbdalhlym067@gmail.com>
2025-10-03 14:43:01 +0300

    Blog Laravel1

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        $categorys = Category::paginate(1);
+        $categorys = Category::paginate(5);
         return view("admin.categories.index", ["categorys" => $categorys]);
     }
 
